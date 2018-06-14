677e546dd39d0c5227272908ac886d08693321f8
Admin <admin@mac-admin.local>
2018-06-14 19:29:20 +0300

    Initial Commit

---
+++ NSDateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDateTest
+//
+//  Created by Admin on 14.06.2018.
+//  Copyright © 2018 SergioLechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
